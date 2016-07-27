4132f7e554ec63ab4573ae848bacc009da3954df
Kevin Clarens <k.clarens@adveez.com>
2016-07-27 21:08:38 +0200

    docs(examples): add a new basic example

--- examples/basic.js
+++ examples/basic.js
@@ -1,5 +1,6 @@
-var StringAlternative = require('../lib/index');
+var StringAlternative = require('../index');
 
+/* zero, one and many example */
 for(var i=0; i<=10; i++) {
   var sa = StringAlternative()
     .count(i)
@@ -9,4 +10,18 @@ for(var i=0; i<=10; i++) {
 
   console.log(sa.toString());
 }
+
+/* default, prefix and suffix example */
+
+for(var i=0; i<=10; i++) {
+  var sa = StringAlternative()
+    .default(' result')
+    .prefix('I found ' + i)
+    .many(' results')
+    .suffix(' matching with you research !')
+    .count(i);
+
+  console.log(sa.toString());
+}
+
 
